2880131786ea4d5b646aa2b1144ad448cd744c86
victorN <victornguyen082@gmail.com>
2019-09-18 07:53:10 -0400

    attempt on lru cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,25 +1,42 @@
-class LRUCache:
-  """
-  Our LRUCache class keeps track of the max number of nodes it
-  can hold, the current number of nodes it is holding, a doubly-
-  linked list that holds the key-value entries in the correct 
-  order, as well as a storage dict that provides fast access
-  to every node stored in the cache.
-  """
-  def __init__(self, limit=10):
-    pass
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('./doubly_linked_list')
 
-  """
+
+class LRUCache:
+    """
+    Our LRUCache class keeps track of the max number of nodes it
+    can hold, the current number of nodes it is holding, a doubly-
+    linked list that holds the key-value entries in the correct 
+    order, as well as a storage dict that provides fast access
+    to every node stored in the cache.
+    """
+
+    def __init__(self, limit=10):
+        self.max_nodes = limit
+        self.count = 0
+        self.current_node = None
+        self.cache = {}
+        self.storage = DoublyLinkedList()
+
+    """
   Retrieves the value associated with the given key. Also
   needs to move the key-value pair to the end of the order
   such that the pair is considered most-recently used.
   Returns the value associated with the key or None if the
   key-value pair doesn't exist in the cache. 
   """
-  def get(self, key):
-    pass
 
-  """
+    def get(self, key):
+        if self.count == 0 or key not in self.cache:
+            return None
+        elif key in self.cache:
+            current_node = self.cache[key]
+            self.storage.delete(current_node)
+            self.storage.add_to_head([key, current_node])
+            return current_node
+
+    """
   Adds the given key-value pair to the cache. The newly-
   added pair should be considered the most-recently used
   entry in the cache. If the cache is already at max capacity
@@ -29,5 +46,18 @@ class LRUCache:
   want to overwrite the old value associated with the key with
   the newly-specified value. 
   """
-  def set(self, key, value):
-    pass
+
+    def set(self, key, value):
+        if key in self.cache:
+            current_node = self.cache[key]
+            self.storage.delete(current_node)
+            self.storage.add_to_head([key, value])
+            self.cache[key] = [value, self.storage.head]
+            return
+
+            if self.max_nodes is self.count:
+                self.storage.remove_from_tail()
+
+            self.storage.add_to_head([key, value])
+            self.cache[key] = [value, self.storage.head]
+            self.count += 1
